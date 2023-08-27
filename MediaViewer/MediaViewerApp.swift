29ddd90e3fdc22ce744eee3deea8eee1078e241b
Eugene Honcharenko <tulusheg@gmail.com>
2023-08-27 10:27:10 +0200

    Initial Commit

---
+++ MediaViewer/MediaViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaViewerApp.swift
+//  MediaViewer
+//
+//  Created by Eugene Honcharenko on 27.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MediaViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
